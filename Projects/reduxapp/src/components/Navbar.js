7d95704c8fc4660a516fd5dfe7272546b54fff20
rohan-movaliya <rohanmovaliya65@gmail.com>
2026-02-15 14:43:01 +0530

    Add reduxapp code

---
+++ Projects/reduxapp/src/components/Navbar.js
@@ -0,0 +1,46 @@
+import { useSelector } from "react-redux";
+
+
+const Navbar = () => {
+
+  const amount = useSelector((state) => state.amount);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Rohan's Bank
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="btn btn-primary" style={{width:"10%"}} disabled={true}>Balance: ₹{amount}</div>
+    </nav>
+  );
+};
+
+
+export default Navbar;
